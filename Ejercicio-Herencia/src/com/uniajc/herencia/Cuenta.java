4dda714336e20e78624255a8aedd6ae9eae1c72c
CarlosCamacho222 <cdcamacho30@gmail.com>
2025-03-04 20:46:47 -0500

    Creamos cuenta Constructor y metodo imprimir +Comentarios

--- Ejercicio-Herencia/src/com/uniajc/herencia/Cuenta.java
+++ Ejercicio-Herencia/src/com/uniajc/herencia/Cuenta.java
@@ -1,29 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.uniajc.herencia;
 
-/**
- *
- * @author Carlos Camacho
- */
 public class Cuenta {
+
     protected float saldo;
     protected int numConsignaciones = 0;
     protected int numRetiros = 0;
-    protected float tasaAnual;
-    protected float comision = 0;
+    protected float tasaAnual; //Porcentaje
+    protected float comisionMensual = 0;
 
+    // creamos constructor
     public Cuenta(float saldo, float tasaAnual) {
         this.saldo = saldo;
         this.tasaAnual = tasaAnual;
     }
+
+    //Creamos metodo para imprimir los atributos
     public void imprimir() {
         System.out.println("Saldo:" + saldo);
         System.out.println("Numero de consignaciones: " + numConsignaciones);
         System.out.println("Numero de retiros: " + numRetiros);
         System.out.println("Tasa Anual: " + tasaAnual);
-        System.out.println("Comision: " + comision);
+        System.out.println("Comision: " + comisionMensual);
     }
 }
